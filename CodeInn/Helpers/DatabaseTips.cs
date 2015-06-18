9f2f200671f32903bc3cd94dd4a475d58b09272f
Saksham Sharma <saksham0808@gmail.com>
2015-06-18 17:17:00 +0530

    New files tracked.

---
+++ CodeInn/Helpers/DatabaseTips.cs
@@ -0,0 +1,84 @@
+﻿using SQLite;
+using CodeInn.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeInn.Helpers
+{
+    public class DatabaseTip
+    {
+        SQLiteConnection dbConn;
+
+        //Create Tabble 
+        public async Task<bool> onCreate(string DB_PATH)
+        {
+            try
+            {
+                if (!CheckFileExists(DB_PATH).Result)
+                {
+                    using (dbConn = new SQLiteConnection(DB_PATH))
+                    {
+                        dbConn.CreateTable<Examples>();
+                        dbConn.CreateTable<Tips>();
+                        dbConn.CreateTable<Problems>();
+                        dbConn.CreateTable<Lessons>();
+                    }
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        private async Task<bool> CheckFileExists(string fileName)
+        {
+            try
+            {
+                var store = await Windows.Storage.ApplicationData.Current.LocalFolder.GetFileAsync(fileName);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        //--------------------READ---------------------//
+        public Tips ReadTip(int tipid)
+        {
+            using (var dbConn = new SQLiteConnection(App.DB_PATH))
+            {
+                var existingtip = dbConn.Query<Tips>("select * from Tips where Id =" + tipid).FirstOrDefault();
+                return existingtip;
+            }
+        }
+
+        // Retrieve the all tip list from the database. 
+        public ObservableCollection<Tips> ReadTips()
+        {
+            using (var dbConn = new SQLiteConnection(App.DB_PATH))
+            {
+                List<Tips> myCollection = dbConn.Table<Tips>().ToList<Tips>();
+                ObservableCollection<Tips> TipsList = new ObservableCollection<Tips>(myCollection);
+                return TipsList;
+            }
+        }
+
+        //--------------------CREATE--------------------//
+        public void InsertTip(Tips newobj)
+        {
+            using (var dbConn = new SQLiteConnection(App.DB_PATH))
+            {
+                dbConn.RunInTransaction(() =>
+                {
+                    dbConn.Insert(newobj);
+                });
+            }
+        }
+    }
+}
---
+++ CodeInn/Views/ExampleViewer.xaml.cs
@@ -0,0 +1,134 @@
+﻿using CodeInn.Common;
+using CodeInn.Helpers;
+using CodeInn.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace CodeInn.Views
+{
+    /// <summary>
+    /// The menu page to retrieve and display Examples
+    /// </summary>
+    public sealed partial class ExampleViewer : Page
+    {
+        private NavigationHelper navigationHelper;
+        private ObservableDictionary defaultViewModel = new ObservableDictionary();
+        
+        ObservableCollection<Examples> DB_ExampleList = new ObservableCollection<Examples>();
+        public ExampleViewer()
+        {
+            this.InitializeComponent();
+
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += this.NavigationHelper_LoadState;
+            this.navigationHelper.SaveState += this.NavigationHelper_SaveState;
+            this.Loaded += ReadExamples_Loaded;
+        }
+
+        private void ReadExamples_Loaded(object sender, RoutedEventArgs e)
+        {
+            ReadExamples dbproblems = new ReadExamples();
+            DB_ExampleList = dbproblems.GetAllExamples();
+            listBox.ItemsSource = DB_ExampleList.OrderByDescending(i => i.Id).ToList();
+        }
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+        public ObservableDictionary DefaultViewModel
+        {
+            get { return this.defaultViewModel; }
+        }
+
+        private void NavigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+        }
+
+        private void NavigationHelper_SaveState(object sender, SaveStateEventArgs e)
+        {
+        }
+
+        #region NavigationHelper registration
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            this.navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            this.navigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
+
+
+        async private void GetDataFromWeb()
+        {
+            var client = new HttpClient();
+            Windows.Storage.ApplicationDataContainer localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+
+            if (!localSettings.Containers.ContainsKey("userInfo"))
+            {
+                MessageDialog msgbox = new MessageDialog("Please log-in first. Go to settings from the main menu.");
+                await msgbox.ShowAsync();
+                return;
+            }
+
+            var lastcheck = localSettings.Containers["userInfo"].Values["lastcheckexamples"].ToString();
+            Debug.WriteLine(System.Uri.EscapeUriString(lastcheck));
+            var response = await client.GetAsync(new Uri("http://ws.varstack.com/time.php?Timestamp=" + System.Uri.EscapeUriString(lastcheck) + "&Table=Examples&Category=easy"));
+
+            while (response.StatusCode == HttpStatusCode.TemporaryRedirect)
+            {
+                response = await client.GetAsync(response.Headers.Location);
+            }
+            var result = await response.Content.ReadAsStringAsync();
+            result = result.Replace("\"", string.Empty);
+            Debug.WriteLine(result);
+
+            List<Examples> newex = JsonConvert.DeserializeObject<List<Examples>>(result);
+            foreach(Examples ex in newex)
+            {
+                DatabaseExample Db_Helper = new DatabaseExample();
+                Db_Helper.InsertExample(ex);
+            }
+
+            localSettings.Containers["userInfo"].Values["lastcheckexamples"] = DateTime.Now.ToString("yyyy-mm-dd hh:mm:ss");
+        }
+
+        private void Refresh_Lessons(object sender, RoutedEventArgs e)
+        {
+            GetDataFromWeb();
+            ReadExamples dbproblems = new ReadExamples();
+            DB_ExampleList = dbproblems.GetAllExamples();
+            listBox.ItemsSource = DB_ExampleList.OrderByDescending(i => i.Id).ToList();
+        }
+
+    }
+}
