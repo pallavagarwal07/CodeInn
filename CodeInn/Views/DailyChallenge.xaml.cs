d16fb64d4d69ee8bb1dd120471ebd898142f511a
Pallav Agarwal <pallavagarwal07@gmail.com>
2015-07-13 20:33:17 +0530

    Structure of DailyChallenge Class made

--- CodeInn/Views/DailyChallenge.xaml.cs
+++ CodeInn/Views/DailyChallenge.xaml.cs
@@ -9,6 +9,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
+using Windows.Networking;
 using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading;
@@ -35,7 +36,7 @@ namespace CodeInn.Views
         private NavigationHelper navigationHelper;
         private ObservableDictionary defaultViewModel = new ObservableDictionary();
         private StatusBarProgressIndicator progressbar;
-
+        private bool isTimeRunning = false;
         ObservableCollection<Examples> DB_ExampleList = new ObservableCollection<Examples>();
         public DailyChallenge()
         {
@@ -43,20 +44,10 @@ namespace CodeInn.Views
             this.navigationHelper = new NavigationHelper(this);
             this.navigationHelper.LoadState += this.NavigationHelper_LoadState;
             this.navigationHelper.SaveState += this.NavigationHelper_SaveState;
-            this.Loaded += ReadExamples_Loaded;
-        }
-
-        private void ReadExamples_Loaded(object sender, RoutedEventArgs e)
-        {
-            ReadExamples dbproblems = new ReadExamples();
-            DB_ExampleList = dbproblems.GetAllExamples();
-            listBox.ItemsSource = DB_ExampleList.OrderBy(i => i.Id).ToList();
-        }
-        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            Examples clickedExample = (Examples)(sender as ListBox).SelectedItem;
-            var navCont = new CodeEditorContext(clickedExample, "Examples");
-            Frame.Navigate(typeof(CodeEditor), navCont);
+            if (!isTimeRunning)
+            {
+                this.WriteTime();
+            }
         }
 
         public NavigationHelper NavigationHelper
@@ -77,7 +68,7 @@ namespace CodeInn.Views
         {
         }
 
-        #region NavigationHelper registration
+#region NavigationHelper registration
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
@@ -90,67 +81,38 @@ namespace CodeInn.Views
             this.navigationHelper.OnNavigatedFrom(e);
         }
 
-        #endregion
+#endregion
 
 
         async private void GetDataFromWeb()
         {
-            progressbar.Text = "Fetching new data";
-            progressbar.ShowAsync();
-
-            var client = new HttpClient();
-            Windows.Storage.ApplicationDataContainer localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
-
-            if (!localSettings.Containers.ContainsKey("userInfo"))
-            {
-                MessageDialog msgbox = new MessageDialog("Please log-in first. Go to settings from the main menu.");
-                await msgbox.ShowAsync();
-                return;
-            }
-
-            var lastcheck = localSettings.Containers["userInfo"].Values["lastcheckexamples"].ToString();
-            Debug.WriteLine(System.Uri.EscapeUriString(lastcheck));
-            var response = await client.GetAsync(new Uri("http://codeinn-acecoders.rhcloud.com:8000/query/data?Timestamp=" + System.Uri.EscapeUriString(lastcheck) + "&Table=Examples"));
-
-            var result = await response.Content.ReadAsStringAsync();
-
-            result = result.Trim(new Char[]{'"'});
-            Debug.WriteLine(result);
-
-            DatabaseExample Db_Helper = new DatabaseExample();
-            try
-            {
-                List<Examples> newex = JsonConvert.DeserializeObject<List<Examples>>(result);
-                foreach (Examples ex in newex)
-                {
-                    try
-                    {
-                        Db_Helper.InsertExample(ex);
-                    }
-                    catch
-                    {
-                        Debug.WriteLine("DB error for item of id: " + ex.Id);
-                    }
-                }
-
-                localSettings.Containers["userInfo"].Values["lastcheckexamples"] = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
-                progressbar.Text = "New items";
-            }
-            catch
-            {
-                Debug.WriteLine("No new items");
-                progressbar.Text = "No New items";
-            }
-            finally
-            {
-                ReadExamples dbproblems = new ReadExamples();
-                DB_ExampleList = dbproblems.GetAllExamples();
-                listBox.ItemsSource = DB_ExampleList.OrderByDescending(i => i.Id).ToList();
-            }
-            progressbar.HideAsync();
         }
 
-        private void Refresh_Examples(object sender, RoutedEventArgs e)
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DailyChallenge clickedExample = (DailyChallenge)(sender as ListBox).SelectedItem;
+            //var navCont = new CodeEditorContext(clickedExample, "Examples");
+            //Frame.Navigate(typeof(CodeEditor), navCont);
+        }
+
+        async private void WriteTime()
+        {
+            DateTime currentDate = DateTime.Now;
+            DateTime tomorrow = new DateTime(currentDate.Year, currentDate.Month, currentDate.Day + 1);
+            double totalTime = (tomorrow.Ticks - currentDate.Ticks) / 10000000.0 / 60;
+            TimeRemaining.Text = "Minutes to submission: " + String.Format("{0:0.000}", totalTime);
+            isTimeRunning = true;
+            while(totalTime > 0)
+            {
+                currentDate = DateTime.Now;
+                totalTime = (tomorrow.Ticks - currentDate.Ticks) / 10000000.0 / 60;
+                TimeRemaining.Text = "Minutes to submission: " + String.Format("{0:0.000}", totalTime);
+                await Task.Delay(50);
+            }
+            //AutoSubmit();
+        }
+
+        private void Refresh_Challenge(object sender, RoutedEventArgs e)
         {
             GetDataFromWeb();
         }
